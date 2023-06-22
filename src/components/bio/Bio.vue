255d5dbe5c8a1db6d93e91f7bb55b002dcb984b2
Jason Wang <blahblahj.wsy@gmail.com>
2023-06-21 21:22:20 -0700

    Jun 2023 bio update

--- src/components/bio/Bio.vue
+++ src/components/bio/Bio.vue
@@ -2,45 +2,66 @@
   <div class="bio-container">
     <Paragraph title="Biography" class="biography">
       <p>
-        I was born in Ottawa, Canada, and moved to
-        <a href="https://en.wikipedia.org/wiki/Shenzhen">Shenzhen</a>, China
-        when I was 3 years old (before I could properly remember things). At a
-        technology event near my home, I learned about programming for the first
-        time, and started building robots in 2010. I graduated from Shenzhen
-        Middle School in 2019 and was lucky enough to be admitted to
-        <a href="https://eecs.berkeley.edu">UC Berkeley</a> Class of 2023 as a
-        Computer Science student. As a (rising) junior student now, I'm an
-        Undergraduate Student Instrctor at this amazing course
-        <a href="https://inst.eecs.berkeley.edu/~cs188/">CS 188</a>
-        (Introduction to Artificial Intelligence). I'm also proud to work as a
-        Reader & Tutor for <a href="https://cs170.org">CS 170</a> (Efficient
-        Algorithms and Intractable Problems) in my sophomore year.
+        Jason was born in Ottawa, Canada, and moved to
+        <a href="https://en.wikipedia.org/wiki/Shenzhen">Shenzhen</a>, China 
+        at the age of 3. He spent his next 15 wonderful years there, all the way 
+        from kindergarten to high school.
+      </p>
+      <p>
+        For college, Jason came to America to study Computer Science as an undergraduate at 
+        <a href="https://eecs.berkeley.edu">UC Berkeley</a> Class of 2022. 
+        During his 3.5 years of undergraduate career, Jason worked as an 
+        undergraduate student instructor for 2 years, and he was the head TA for a 
+        600-student AI course <a href="https://inst.eecs.berkeley.edu/~cs188/">CS 188</a>. 
+        Besides teaching, Jason was an AI researcher in 
+        <a href="https://bair.berkeley.edu">BAIR</a> supervised by Professor Sergey Levine. 
+        Jason graduated from UC Berkeley with full 4.0 GPA and won the 
+        <a href="https://www2.eecs.berkeley.edu/Students/Awards/21/">Mark D. Weiser Excellence 
+        in Computing Scholarship Award</a> as well as the 
+        <a href="https://lsadvising.berkeley.edu/policies/deans-listhonors">Highest Distinction 
+        in General Scholarship</a> (summa cum laude).
+      </p>
+      <p>
+        For work, Jason interned at <a href="https://www.nvidia.com/en-us/">NVIDIA</a> 
+        twice for nearly a year as a Deep Learning Algorithm Engineer. He helped develop 
+        many AI models and tools for both NVIDIA's customers and internal use.
+      </p>
+      <p>
+        Jason's next step is to pursue his MSCS degree at 
+        <a href="https://cs.stanford.edu">Stanford</a> where he will focus on AI. 
+        He hopes to get exposed to more cutting-edge AI technology and research in order 
+        to be more successful in his future career.
       </p>
     </Paragraph>
     <hr />
     <Paragraph title="Interest" class="interest">
       <p>
-        I'm very interested in Deep Learning, especially Deep Reinforcement
-        Learning - it's very fascinating to see an agent learn to improve its
-        performance just by itself. I enjoy doing "weird" projects whenever I
-        have interesting ideas (see <a href="#">project</a> section). I also
-        play fingerstyle guitar and sometimes produce music (if I really have
-        the time to do so). Not sure if I'll still be making videos, but I'll
-        take that as one of my interests as well :(
+        Jason is very interested in Deep Learning. He enjoys doing "weird" 
+        projects whenever he has interesting ideas (see <a href="#">project</a> section). 
+        Jason loves music - not just listening but also playing and producing music. 
+        He can play piano and fingerstyle guitar, and he sometimes create his own music.
       </p>
     </Paragraph>
     <Paragraph title="Skills" class="skills">
       <p>
-        For Machine Learning, I'm fluent in writing Python code, and using DL
-        frameworks such as <a href="https://pytorch.org">PyTorch</a> and
-        <a href="https://www.tensorflow.org">TensorFlow</a>. <br />
-        For Software Engineering, I could use Python, Java, C/C++ depending on
-        the actual task. <br />
-        For Web Development, I had full stack developing experience using
+        For Machine Learning, Jason is very fluent in writing Python code, and familiar 
+        with using popular DL platforms like <a href="https://pytorch.org">PyTorch</a> 
+        to build and train models as well as exporting models to other formats like 
+        <a href="https://github.com/NVIDIA/TensorRT">TensorRT</a> to maximize inference performance for deployment. He also 
+        had experience writing lower level customized kernels using 
+        <a href="https://openai.com/research/triton">OpenAI Triton</a> to achieve best 
+        performance for specific workflows.
+      </p>
+      <p>
+        For Software Engineering, Jason could use Python, Java, C/C++ depending on
+        the actual task.
+      </p>
+      <p>
+        For Web Development, Jason had full stack developing experience using
         <a href="https://www.djangoproject.com">Django</a> &
-        <a href="https://reactjs.org">React.js</a>. I could also build websites
+        <a href="https://reactjs.org">React.js</a>. He could also build websites
         using <a href="https://vuejs.org">Vue.js</a> (as that's how this website
-        is built with).
+        is built).
       </p>
     </Paragraph>
     <hr />
